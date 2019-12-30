33b9bd349592315ee8ffdcad38e4d1f4b094ea2f
mrjorgexavier <mr.jorge.xavier>
2019-12-29 22:45:46 -0300

    Iniciando implementação do GamePlay manager, que vai gerenciar os componentes do jogo

---
+++ Assets/Scripts/GameplayManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System;
+
+public class GameplayManager: MonoBehaviour {
+    [SerializeField] public ProjectileController projectileController;
+    [SerializeField] public MainCameraController mainCameraController;
+    [SerializeField] public AimCameraController aimCameraController;
+
+    void Update() {
+        
+    }
+}
--- Assets/Scripts/Gravity.cs
+++
@@ -1,5 +0,0 @@
-using UnityEngine;
-
-public class Gravity: MonoBehaviour {
-    
-}
